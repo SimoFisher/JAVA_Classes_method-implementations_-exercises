f4ee0815fd3a7816eb68c72badf943908242005e
SimoFisher <120340011+SimoFisher@users.noreply.github.com>
2022-12-13 13:15:17 +0100

    Update Test_stream_Example1.java

--- Test_stream_Example1.java
+++ Test_stream_Example1.java
@@ -7,7 +7,7 @@ public class Test_stream_Example1 {
 public static void main(String[] args) {
 	 Supplier<Stream <String>> stringSupplier= ()->Stream.of("z", "f","b","a").filter(s -> s.startsWith("a"));
 	 System.out.println(stringSupplier.get().anyMatch(s->true));
-	 // The stringSupplier is a Supplier that get a Stram of Strings, and return True
+	 // The stringSupplier is a Supplier that get a Stream of Strings, and return True
 	 // if there is a match with the filter() function
 	 System.out.println(stringSupplier.get().noneMatch(s->true));
 	// returns true if every x is different from other elements x_1,x_2..x_n for all elements
